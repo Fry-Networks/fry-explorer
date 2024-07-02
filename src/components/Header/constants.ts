ab91f77849dcc5631520a2cd66b377b8722db24b
Simon <44496148+siim0n@users.noreply.github.com>
2024-07-02 17:16:29 +0200

    Initial commit

---
+++ src/components/Header/constants.ts
@@ -0,0 +1,8 @@
+export const NAVIGATION_LINKS = [
+  { title: "Miners Map", href: "/", layoutSegments: [null, "hex", "mobile"] },
+  {
+    title: "Miners Stats",
+    href: "/stats",
+    layoutSegments: ["stats"],
+  },
+]
