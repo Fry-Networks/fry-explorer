77db60dd663675afa5fb2a54ff6a02b1057a9401
Simon <44496148+siim0n@users.noreply.github.com>
2024-08-21 13:09:05 +0200

    fix logos

--- src/components/icons/FryIcon.tsx
+++ src/components/icons/FryIcon.tsx
@@ -1,10 +1,14 @@
+"use client"
+import { useTheme } from "next-themes"
 import Image from "next/image"
 
 export function FryIcon(props: { className?: string }) {
+  const { resolvedTheme } = useTheme()
+  console.log(resolvedTheme)
   return (
     //import the logo.svg file from the public folder
     <Image
-      src="/logo.svg"
+      src={resolvedTheme == "light" ? "/logo_light.svg" : "/logo_dark.svg"}
       alt="Fry"
       width={2}
       height={100}
